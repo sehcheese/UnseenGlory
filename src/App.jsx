e76588939a3bc79f7dd0c0a1f055654c33c76433
Scott Hoelsema <sehcheese@gmail.com>
2018-11-24 11:58:09 -0500

    New files

---
+++ src/App.jsx
@@ -0,0 +1,214 @@
+import React, { Component } from 'react';
+import { MuiThemeProvider, createMuiTheme, withStyles } from '@material-ui/core/styles';
+import blueGrey from '@material-ui/core/colors/blueGrey';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import BookIcon from '@material-ui/icons/Book';
+import Hidden from '@material-ui/core/Hidden';
+import Home from './Home';
+import Poem from './Poem';
+
+import { poems, poemOrder } from './poems';
+import { unseenGloryColors, randomUnseenGloryColor } from './unseenGloryColors';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  appBarTitle: {
+    width: '100%',
+    textAlign: 'center',
+  },
+  appBarTitleElement: {
+    display: 'inline',
+    verticalAlign: 'middle',
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  title: {
+    verticalAlign: 'middle',
+  },
+};
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#000",
+    },
+    secondary: blueGrey,
+  },
+  typography: {
+    fontFamily: '"Bitter", serif',
+  },
+});
+
+const white = '#FFF';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    const unFragmentColor = randomUnseenGloryColor();
+    const seenFragmentColor = randomUnseenGloryColor(unFragmentColor);
+    const gloryFragmentColor = unseenGloryColors.filter(color => color !== unFragmentColor && color !== seenFragmentColor)[0];
+
+    this.state = {
+      chevronColorLeft: white,
+      chevronColorRight: white,
+      unFragmentColor,
+      seenFragmentColor,
+      gloryFragmentColor,
+      bookIconColor: white,
+      activePoemIndex: null,
+      showReferences: false,
+    }
+  }
+
+  advancePoemLeft = () => {
+    const activePoemIndex = this.state.activePoemIndex !== null ? this.state.activePoemIndex : 0;
+    const newActivePoemIndex = activePoemIndex === 0 ? poemOrder.length - 1 : activePoemIndex - 1;
+    this.setState(() => ({ activePoemIndex: newActivePoemIndex }));
+  }
+
+  advancePoemRight = () => {
+    const activePoemIndex = this.state.activePoemIndex !== null ? this.state.activePoemIndex : -1;
+    this.setState(() => ({ activePoemIndex: (activePoemIndex + 1) % poemOrder.length }));
+  }
+
+  onEnterChevronLeft = () => {
+    this.setState(() => ({ chevronColorLeft: randomUnseenGloryColor(this.state.chevronColorLeft) }));
+  }
+
+  onLeaveChevronLeft = () => {
+    this.setState(() => ({ chevronColorLeft: white }));
+  }
+
+  onEnterChevronRight = () => {
+    this.setState(() => ({ chevronColorRight: randomUnseenGloryColor(this.state.chevronColorRight) }));
+  }
+
+  onLeaveChevronRight = () => {
+    this.setState(() => ({ chevronColorRight: white }));
+  }
+
+  onEnterTitleFragment = () => {
+    const unFragmentColor = randomUnseenGloryColor(this.state.unFragmentColor);
+    const seenFragmentColor = randomUnseenGloryColor(unFragmentColor);
+    const gloryFragmentColor = unseenGloryColors.filter(color => color !== unFragmentColor && color !== seenFragmentColor)[0];
+
+    this.setState(() => ({
+      unFragmentColor,
+      seenFragmentColor,
+      gloryFragmentColor,
+    }));
+  }
+
+  onEnterShowReferences = () => {
+    this.setState(() => ({ bookIconColor: randomUnseenGloryColor(this.state.bookIconColor) }));
+  }
+
+  onLeaveShowReferences = () => {
+    if (!this.state.showReferences) {
+      this.setState(() => ({ bookIconColor: white }));
+    }
+  }
+
+  onClickShowReferences = () => {
+    this.setState(() => ({
+      showReferences: !this.state.showReferences,
+      bookIconColor: randomUnseenGloryColor(this.state.bookIconColor),
+    }));
+  }
+
+  render() {
+    const { classes } = this.props;
+
+    let content;
+    if (this.state.activePoemIndex !== null) {
+      content = <Poem activePoem={poems[poemOrder[this.state.activePoemIndex]]} showReferences={this.state.showReferences} />;
+    } else {
+      content = <Home />
+    }
+
+    return (
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <div className={classes.appBarTitle}>
+              <Hidden smDown>
+                <IconButton
+                  className={classes.appBarTitleElement}
+                  color="inherit" aria-label="Left"
+                  onMouseEnter={this.onEnterChevronLeft}
+                  onMouseLeave={this.onLeaveChevronLeft}
+                  onClick={this.advancePoemLeft}
+                >
+                  <ChevronLeft style={{color: this.state.chevronColorLeft}} fontSize="large" alignmentBaseline="middle" />
+                </IconButton>
+              </Hidden>
+              <Typography className={classes.appBarTitleElement} variant="h5" color="inherit" align="center">
+                <span
+                  className={classes.title}
+                  style={{color: this.state.unFragmentColor}}
+                  onMouseEnter={this.onEnterTitleFragment}
+                >
+                  UN
+                </span>
+                <span
+                  className={classes.title}
+                  style={{color: this.state.seenFragmentColor}}
+                  onMouseEnter={this.onEnterTitleFragment}
+                >
+                  SEEN
+                </span>
+                <span className={classes.title}> </span>
+                <span
+                  className={classes.title}
+                  style={{color: this.state.gloryFragmentColor}}
+                  onMouseEnter={this.onEnterTitleFragment}
+                >
+                  GLORY
+                </span>
+              </Typography>
+              <Hidden smDown>
+                <IconButton
+                  className={classes.appBarTitleElement}
+                  color="inherit" aria-label="Right"
+                  onMouseEnter={this.onEnterChevronRight}
+                  onMouseLeave={this.onLeaveChevronRight}
+                  onClick={this.advancePoemRight}
+                >
+                  <ChevronRight style={{color: this.state.chevronColorRight}} fontSize="large" alignmentBaseline="middle" />
+                </IconButton>
+              </Hidden>
+            </div>
+            <IconButton
+              color="inherit"
+              aria-label="Right"
+              onMouseEnter={this.onEnterShowReferences}
+              onMouseLeave={this.onLeaveShowReferences}
+              onClick={this.onClickShowReferences}
+            >
+              <BookIcon style={{color: this.state.bookIconColor}} alignmentBaseline="middle" />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        {content}
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default withStyles(styles)(App);
---
+++ src/Home.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core';
+
+const styles = {
+  home: {
+    maxWidth: '700px',
+    margin: 'auto',
+  },
+  homeContent: {
+    marginTop: '20px',
+    textAlign: 'center',
+  },
+  quote: {
+    fontStyle: 'italic',
+  },
+};
+
+class Home extends Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.home}>
+        <Typography className={classes.homeContent}>
+          poems by Scott Hoelsema
+        </Typography>
+        <Typography className={classes.homeContent}>
+          <span className={classes.quote}>"And we all, who with unveiled faces contemplate the Lord’s glory, are being transformed into his image with ever-increasing glory, which comes from the Lord, who is the Spirit."</span><br />
+          2 Corinthians 3:18
+        </Typography>
+        <Typography className={classes.homeContent}>
+          <span className={classes.quote}>"Though you have not seen him, you love him; and even though you do not see him now, you believe in him and are filled with an inexpressible and glorious joy."</span><br />
+          1 Peter 1:8
+        </Typography>
+        <Typography className={classes.homeContent}>
+          <span className={classes.quote}>"The glory must be believed."</span><br />
+          Søren Kierkegaard
+        </Typography>
+        <Typography className={classes.homeContent}>
+          If this poetry seems didactic, it is only because I am instructing my own soul.<br />
+          If this poetry seems dogmatic, it is only because I am convincing myself.<br />
+          If this poetry seems enigmatic, it is only because I grasp at mysteries.<br />
+          If this poetry seems poetic, perhaps I've succeeded.<br />
+        </Typography>
+        <Typography className={classes.homeContent}>
+          Referring to Christ, Kierkegaard said, "the glory must be believed." So it is too for his followers, who share in his suffering, comfort, and glory. Ours too is the lowliness of the manger and the cross - a life dense with unseen glory. It must be believed not because it does not exist, but because often it is hard to see, to reconcile with our circumstances.<br /><br />
+          Today, Christmas Eve 2018, I have released publicly poetry from the last three years to the Unseen Glory collection. I'd be honored if you checked it out, and am always glad to talk about it. The site now has a toggle to reveal references to influencing sources. Thank you, friends, for helping me to believe the glory.
+        </Typography>
+        <Typography className={classes.homeContent}>
+          Thanks to <a href="https://reactjs.org/">React</a>, <a href="https://github.com/facebook/create-react-app">Create React App</a>, <a href="https://reacttraining.com/react-router/">React Router</a>, and <a href="https://material-ui.com/">Material-UI</a> for making it easy to make this website. See the code on <a href="https://github.com/sehcheese/UnseenGlory">Github</a>.
+        </Typography>
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(Home);
