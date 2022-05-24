f5c9467b4ce871042ad058f4dbdab44d2e855ba2
Scotty Hoelsema <sehcheese@gmail.com>
2022-05-23 23:01:34 -0400

    Add poem "Homecoming"

--- src/poems.js
+++ src/poems.js
@@ -1,172 +1,175 @@
-import aPrayer from './poems/aPrayer';
-import azaleaBlooming from './poems/azaleaBlooming';
-import callAndResponse from './poems/callAndResponse';
-import cirrus from './poems/cirrus';
-import coramDeo from './poems/coramDeo';
-import easter from './poems/easter';
-import forgottenFlowers from './poems/forgottenFlowers';
-import freeAtLast from './poems/freeAtLast';
-import gloriousThingsEscapeUs from './poems/gloriousThingsEscapeUs';
-import halcyonDreams from './poems/halcyonDreams';
-import hallelujah from './poems/hallelujah';
-import horizon from './poems/horizon';
-import inspired from './poems/inspired';
-import kapooya from './poems/kapooya';
-import letUsGoWhereTheWoundIsDeep from './poems/letUsGoWhereTheWoundIsDeep';
-import liftUpYourHearts from './poems/liftUpYourHearts';
-import listen from './poems/listen';
-import logos from './poems/logos';
-import lordOfTheBalance from './poems/lordOfTheBalance';
-import loveIsNoAbstraction from './poems/loveIsNoAbstraction';
-import meditationOnJohn14_6 from './poems/meditationOnJohn14_6';
-import mockingbird from './poems/mockingbird';
-import myBirdsTheySingForGlory from './poems/myBirdsTheySingForGlory';
-import myEnd from './poems/myEnd';
-import myTimesAreInYourHand from './poems/myTimesAreInYourHand';
-import myVisitor from './poems/myVisitor';
-import noon from './poems/noon';
-import one from './poems/one';
-import patience from './poems/patience';
-import peaceMySoul from './poems/peaceMySoul';
-import pegasus from './poems/pegasus';
-import prologue from './poems/prologue';
-import rain from './poems/rain';
-import sanctifying from './poems/sanctifying';
-import seasons from './poems/seasons';
-import shepherd from './poems/shepherd';
-import someMoments from './poems/someMoments';
-import spoken from './poems/spoken';
-import still from './poems/still';
-import summons from './poems/summons';
-import taken from './poems/taken';
-import theBirdsOnTheWire from './poems/theBirdsOnTheWire';
-import theDeathOfHope from './poems/theDeathOfHope';
-import theLastSongOfTheTuckerbird from './poems/theLastSongOfTheTuckerbird';
-import theLightBeforeTheDawn from './poems/theLightBeforeTheDawn';
-import thisChasmOfUnknowing from './poems/thisChasmOfUnknowing';
-import thisPathIveTread from './poems/thisPathIveTread';
-import toAFriend from './poems/toAFriend';
-import toEveryQuestion from './poems/toEveryQuestion';
-import trinityPrayer from './poems/trinityPrayer';
-import truthBeTold from './poems/truthBeTold';
-import twelveEleven from './poems/twelveEleven';
-import waking from './poems/waking';
-import winterSun from './poems/winterSun';
-import youAreNoFiction from './poems/youAreNoFiction';
+import aPrayer from "./poems/aPrayer";
+import azaleaBlooming from "./poems/azaleaBlooming";
+import callAndResponse from "./poems/callAndResponse";
+import cirrus from "./poems/cirrus";
+import coramDeo from "./poems/coramDeo";
+import easter from "./poems/easter";
+import forgottenFlowers from "./poems/forgottenFlowers";
+import freeAtLast from "./poems/freeAtLast";
+import gloriousThingsEscapeUs from "./poems/gloriousThingsEscapeUs";
+import halcyonDreams from "./poems/halcyonDreams";
+import hallelujah from "./poems/hallelujah";
+import homecoming from "./poems/homecoming";
+import horizon from "./poems/horizon";
+import inspired from "./poems/inspired";
+import kapooya from "./poems/kapooya";
+import letUsGoWhereTheWoundIsDeep from "./poems/letUsGoWhereTheWoundIsDeep";
+import liftUpYourHearts from "./poems/liftUpYourHearts";
+import listen from "./poems/listen";
+import logos from "./poems/logos";
+import lordOfTheBalance from "./poems/lordOfTheBalance";
+import loveIsNoAbstraction from "./poems/loveIsNoAbstraction";
+import meditationOnJohn14_6 from "./poems/meditationOnJohn14_6";
+import mockingbird from "./poems/mockingbird";
+import myBirdsTheySingForGlory from "./poems/myBirdsTheySingForGlory";
+import myEnd from "./poems/myEnd";
+import myTimesAreInYourHand from "./poems/myTimesAreInYourHand";
+import myVisitor from "./poems/myVisitor";
+import noon from "./poems/noon";
+import one from "./poems/one";
+import patience from "./poems/patience";
+import peaceMySoul from "./poems/peaceMySoul";
+import pegasus from "./poems/pegasus";
+import prologue from "./poems/prologue";
+import rain from "./poems/rain";
+import sanctifying from "./poems/sanctifying";
+import seasons from "./poems/seasons";
+import shepherd from "./poems/shepherd";
+import someMoments from "./poems/someMoments";
+import spoken from "./poems/spoken";
+import still from "./poems/still";
+import summons from "./poems/summons";
+import taken from "./poems/taken";
+import theBirdsOnTheWire from "./poems/theBirdsOnTheWire";
+import theDeathOfHope from "./poems/theDeathOfHope";
+import theLastSongOfTheTuckerbird from "./poems/theLastSongOfTheTuckerbird";
+import theLightBeforeTheDawn from "./poems/theLightBeforeTheDawn";
+import thisChasmOfUnknowing from "./poems/thisChasmOfUnknowing";
+import thisPathIveTread from "./poems/thisPathIveTread";
+import toAFriend from "./poems/toAFriend";
+import toEveryQuestion from "./poems/toEveryQuestion";
+import trinityPrayer from "./poems/trinityPrayer";
+import truthBeTold from "./poems/truthBeTold";
+import twelveEleven from "./poems/twelveEleven";
+import waking from "./poems/waking";
+import winterSun from "./poems/winterSun";
+import youAreNoFiction from "./poems/youAreNoFiction";
 
 export const poems = {
-  'a-prayer': aPrayer,
-  'azalea-blooming': azaleaBlooming,
-  'call-and-response': callAndResponse,
+  "a-prayer": aPrayer,
+  "azalea-blooming": azaleaBlooming,
+  "call-and-response": callAndResponse,
   cirrus,
-  'coram-deo': coramDeo,
+  "coram-deo": coramDeo,
   easter,
-  'forgotten-flowers': forgottenFlowers,
-  'free-at-last': freeAtLast,
-  'glorious-things-escape-us': gloriousThingsEscapeUs,
-  'halcyon-dreams': halcyonDreams,
+  "forgotten-flowers": forgottenFlowers,
+  "free-at-last": freeAtLast,
+  "glorious-things-escape-us": gloriousThingsEscapeUs,
+  "halcyon-dreams": halcyonDreams,
   hallelujah,
+  homecoming,
   horizon,
   inspired,
   kapooya,
-  'let-us-go-where-the-wound-is-deep': letUsGoWhereTheWoundIsDeep,
+  "let-us-go-where-the-wound-is-deep": letUsGoWhereTheWoundIsDeep,
   listen,
   logos,
-  'lord-of-the-balance': lordOfTheBalance,
-  'love-is-no-abstraction': loveIsNoAbstraction,
-  'meditation-on-john-14-6': meditationOnJohn14_6,
+  "lord-of-the-balance": lordOfTheBalance,
+  "love-is-no-abstraction": loveIsNoAbstraction,
+  "meditation-on-john-14-6": meditationOnJohn14_6,
   mockingbird,
-  'my-birds-they-sing-for-glory': myBirdsTheySingForGlory,
-  'my-end': myEnd,
-  'my-times-are-in-your-hand': myTimesAreInYourHand,
-  'my-visitor': myVisitor,
+  "my-birds-they-sing-for-glory": myBirdsTheySingForGlory,
+  "my-end": myEnd,
+  "my-times-are-in-your-hand": myTimesAreInYourHand,
+  "my-visitor": myVisitor,
   noon,
   one,
   patience,
-  'peace-my-soul': peaceMySoul,
+  "peace-my-soul": peaceMySoul,
   pegasus,
   prologue,
   sanctifying,
   seasons,
   rain,
   shepherd,
-  'some-moments': someMoments,
+  "some-moments": someMoments,
   spoken,
   still,
   summons,
   taken,
-  'the-birds-on-the-wire': theBirdsOnTheWire,
-  'the-death-of-hope': theDeathOfHope,
-  'the-last-song-of-the-tuckerbird': theLastSongOfTheTuckerbird,
-  'the-light-before-the-dawn': theLightBeforeTheDawn,
-  'this-chasm-of-unknowing': thisChasmOfUnknowing,
-  'this-path-ive-tread': thisPathIveTread,
-  'to-a-friend': toAFriend,
-  'to-every-question': toEveryQuestion,
-  'trinity-prayer': trinityPrayer,
-  'truth-be-told': truthBeTold,
-  'twelve-eleven': twelveEleven,
+  "the-birds-on-the-wire": theBirdsOnTheWire,
+  "the-death-of-hope": theDeathOfHope,
+  "the-last-song-of-the-tuckerbird": theLastSongOfTheTuckerbird,
+  "the-light-before-the-dawn": theLightBeforeTheDawn,
+  "this-chasm-of-unknowing": thisChasmOfUnknowing,
+  "this-path-ive-tread": thisPathIveTread,
+  "to-a-friend": toAFriend,
+  "to-every-question": toEveryQuestion,
+  "trinity-prayer": trinityPrayer,
+  "truth-be-told": truthBeTold,
+  "twelve-eleven": twelveEleven,
   waking,
-  'winter-sun': winterSun,
-  'you-are-no-fiction': youAreNoFiction,
-  'lift-up-your-hearts': liftUpYourHearts,
+  "winter-sun": winterSun,
+  "you-are-no-fiction": youAreNoFiction,
+  "lift-up-your-hearts": liftUpYourHearts,
 };
 
 export const alphabeticallyOrderedPoems = Object.keys(poems).sort();
 
 export const semanticallyOrderedPoems = [
-  'prologue',
-  'mockingbird',
-  'the-light-before-the-dawn',
-  'summons',
-  'horizon',
-  'kapooya',
-  'listen',
-  'still',
-  'love-is-no-abstraction',
-  'shepherd',
-  'the-birds-on-the-wire',
-  'the-last-song-of-the-tuckerbird',
-  'to-every-question',
-  'let-us-go-where-the-wound-is-deep',
-  'sanctifying',
-  'my-times-are-in-your-hand',
-  'my-end',
-  'patience',
-  'this-path-ive-tread',
-  'taken',
-  'a-prayer',
-  'you-are-no-fiction',
-  'hallelujah',
-  'halcyon-dreams',
-  'free-at-last',
-  'trinity-prayer',
-  'easter',
-  'forgotten-flowers',
-  'cirrus',
-  'lord-of-the-balance',
-  'one',
-  'my-visitor',
-  'inspired',
-  'spoken',
-  'logos',
-  'this-chasm-of-unknowing',
-  'the-death-of-hope',
-  'noon',
-  'seasons',
-  'pegasus',
-  'azalea-blooming',
-  'waking',
-  'truth-be-told',
-  'twelve-eleven',
-  'peace-my-soul',
-  'glorious-things-escape-us',
-  'call-and-response',
-  'to-a-friend',
-  'meditation-on-john-14-6',
-  'my-birds-they-sing-for-glory',
-  'coram-deo',
-  'some-moments',
-  'winter-sun',
-  'lift-up-your-hearts',
+  "prologue",
+  "mockingbird",
+  "the-light-before-the-dawn",
+  "summons",
+  "horizon",
+  "kapooya",
+  "listen",
+  "still",
+  "love-is-no-abstraction",
+  "shepherd",
+  "the-birds-on-the-wire",
+  "the-last-song-of-the-tuckerbird",
+  "to-every-question",
+  "let-us-go-where-the-wound-is-deep",
+  "sanctifying",
+  "my-times-are-in-your-hand",
+  "my-end",
+  "patience",
+  "this-path-ive-tread",
+  "taken",
+  "a-prayer",
+  "you-are-no-fiction",
+  "hallelujah",
+  "halcyon-dreams",
+  "free-at-last",
+  "trinity-prayer",
+  "easter",
+  "forgotten-flowers",
+  "cirrus",
+  "lord-of-the-balance",
+  "one",
+  "my-visitor",
+  "inspired",
+  "spoken",
+  "logos",
+  "this-chasm-of-unknowing",
+  "the-death-of-hope",
+  "noon",
+  "seasons",
+  "pegasus",
+  "azalea-blooming",
+  "waking",
+  "truth-be-told",
+  "twelve-eleven",
+  "peace-my-soul",
+  "glorious-things-escape-us",
+  "call-and-response",
+  "to-a-friend",
+  "meditation-on-john-14-6",
+  "my-birds-they-sing-for-glory",
+  "coram-deo",
+  "some-moments",
+  "winter-sun",
+  "lift-up-your-hearts",
+  "homecoming",
 ];
