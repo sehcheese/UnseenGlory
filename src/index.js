857756920a23db4f0863a94cdf96138db9f83f44
Scotty Hoelsema <sehcheese@gmail.com>
2023-01-05 22:28:56 -0500

    Remove unused prop

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App tab="home" />);
+root.render(<App />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
