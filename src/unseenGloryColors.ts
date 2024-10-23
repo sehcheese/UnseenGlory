63a36267ec2fcd35d27ffa4c41a727145c145346
Scotty Hoelsema <sehcheese@gmail.com>
2024-10-23 16:51:51 -0400

    Fix, cleanup, recognize typescript files

--- src/unseenGloryColors.ts
+++ src/unseenGloryColors.ts
@@ -1,17 +1,13 @@
-export const unseenGloryColors = [
-  '#ff6600',
-  '#00aa00',
-  '#2E9AFE'
-];
+export const unseenGloryColors = ["#ff6600", "#00aa00", "#2E9AFE"];
 
-export const randomUnseenGloryColor = (skipColor = null) => {
+export const randomUnseenGloryColor = (skipColor: string | null = null) => {
   const availableColors = [...unseenGloryColors];
   if (skipColor) {
-    var index = availableColors.indexOf(skipColor);
+    const index = availableColors.indexOf(skipColor);
     if (index > -1) {
       availableColors.splice(index, 1);
     }
   }
   const colorClassIndex = Math.floor(Math.random() * availableColors.length);
   return availableColors[colorClassIndex];
-}
+};
